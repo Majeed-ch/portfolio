66d92a47ecd2617f565f1f53f6bfc4d2ec9ffe0d
AbdulMajeed <chowdhury4554@gmail.com>
2023-07-01 14:36:27 -0400

    fix contact form

--- index.js
+++ index.js
@@ -38,21 +38,4 @@ headerLogoConatiner.addEventListener('click', () => {
 })
 
 // ---
-const handleSubmit = (event) => {
-  event.preventDefault();
 
-  const myForm = event.target;
-  const formData = new FormData(myForm);
-  
-  fetch(event.getAttribute('action'), {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => alert("Thank you for your message"))
-    .catch((error) => alert(error));
-};
-
-document
-  .querySelector(".contact__form")
-  .addEventListener("submit", handleSubmit);
